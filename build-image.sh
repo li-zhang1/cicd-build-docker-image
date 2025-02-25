6032a785b5af6e0d8009b6a57cf7c52a4b5c4a5b
Li Zhang <zhangli0623janet@gmail.com>
2025-02-25 08:37:27 -0500

    update files

--- build-image.sh
+++ build-image.sh
@@ -3,5 +3,8 @@
 # fail on any error
 set -eu 
 
+# login to your docker hub account
+docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
+
 # build the docker image
 docker build -f $IMAGE_TAG/Dockerfile -t $IMAGE_TAG .
--- push-image.sh
+++ push-image.sh
@@ -3,9 +3,6 @@
 # fail on any error
 set -eu
 
-# login to your docker hub account
-docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
-
 # use the docker tag command to give the image a new name
 docker tag $IMAGE_TAG $DOCKER_HUB_USERNAME/$DOCKER_HUB_REPO_NAME
 
